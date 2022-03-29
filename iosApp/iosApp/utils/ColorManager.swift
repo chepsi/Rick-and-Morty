ff9c2b08fe212adfd28118f29f6dd3e10ea6b7e8
Le Bon B' Bauma <baumabulondo@gmail.com>
2022-03-30 01:52:00 +0300

    Style: Color management for iOS

---
+++ iosApp/iosApp/utils/ColorManager.swift
@@ -0,0 +1,20 @@
+//
+//  ColorManager.swift
+//  iosApp
+//
+//  Created by Le Bon B' Bauma on 29/03/2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct ColorManager {
+    
+    
+    static let primaryColor = Color("PrimaryColor")
+    static let secondaryColor = Color("SecondaryColor")
+
+    
+}
+
+
