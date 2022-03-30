4075d59c21bea5152ef2774fffdadbaa8f6cfc07
Le Bon B' Bauma <baumabulondo@gmail.com>
2022-03-30 19:15:02 +0300

    Refactor: EpisodeView refactoring

--- iosApp/iosApp/ui/episodes/EpisodesView.swift
+++ iosApp/iosApp/ui/episodes/EpisodesView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct EpisodesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Episodes")
     }
 }
 
