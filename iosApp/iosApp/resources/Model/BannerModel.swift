f090754d33fd442c2a972669813ea465d6417453
Le Bon B' Bauma <baumabulondo@gmail.com>
2022-03-31 04:55:16 +0300

    Build: Coddle extension for reading JSON files

---
+++ iosApp/iosApp/resources/Model/BannerModel.swift
@@ -0,0 +1,16 @@
+//
+//  BannerModel.swift
+//  iosApp
+//
+//  Created by Le Bon B' Bauma on 31/03/2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import Foundation
+
+struct Banner: Codable, Identifiable {
+    
+    let id: Int
+    let image: String
+    
+}
