9992ab62c5c80570fcdf73b4c5c0183de6efc354
Le Bon B' Bauma <baumabulondo@gmail.com>
2022-03-31 04:56:44 +0300

    Feature: Banner for episodes coming Soon

---
+++ iosApp/iosApp/ui/characters/BannerTabview.swift
@@ -0,0 +1,21 @@
+//
+//  BannerTabview.swift
+//  iosApp
+//
+//  Created by Le Bon B' Bauma on 31/03/2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct BannerTabview: View {
+    var body: some View {
+       Text("Hello")
+    }
+}
+
+struct BannerTabview_Previews: PreviewProvider {
+    static var previews: some View {
+        BannerTabview()
+    }
+}
