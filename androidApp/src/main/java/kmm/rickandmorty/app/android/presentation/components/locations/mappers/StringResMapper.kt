1bf51fa21262d50f4c46ed1712fd1152b1f8a81e
Ian Okumu <okumu.otsembo@gmail.com>
2022-05-11 12:31:18 +0300

    Android: Created string resource mapper for dynamic strings outside of UI layer

---
+++ androidApp/src/main/java/kmm/rickandmorty/app/android/presentation/components/locations/mappers/StringResMapper.kt
@@ -0,0 +1,23 @@
+package kmm.rickandmorty.app.android.presentation.components.locations.mappers
+
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+
+sealed class StringResMapper {
+    data class DynamicString(val stringValue:String) : StringResMapper()
+    class StringResource(
+        @StringRes
+        val resID: Int,
+        vararg val args: Any
+    ) : StringResMapper()
+
+    @Composable
+    fun stringAble() : String {
+        return when(this){
+            is DynamicString -> stringValue
+            is StringResource -> stringResource(id = resID, formatArgs = *args)
+        }
+    }
+    
+}
