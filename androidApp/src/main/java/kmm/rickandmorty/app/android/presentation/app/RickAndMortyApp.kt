23fb742a34fc9c5683fdffa9f526e7530199c57c
chepsi <evanschepsiror@gmail.com>
2023-05-27 13:34:18 +0300

    Feature: Update dependencies.

--- androidApp/src/main/java/kmm/rickandmorty/app/android/presentation/app/RickAndMortyApp.kt
+++ androidApp/src/main/java/kmm/rickandmorty/app/android/presentation/app/RickAndMortyApp.kt
@@ -5,8 +5,11 @@ import android.content.Context
 import kmm.rickandmorty.app.android.presentation.components.characters.CharactersViewModel
 import kmm.rickandmorty.app.android.presentation.components.episodes.EpisodesViewModel
 import kmm.rickandmorty.app.android.presentation.components.locations.LocationsViewModel
-import kmm.rickandmorty.app.initKoin
+import kmm.rickandmorty.app.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
 class RickAndMortyApp : Application() {
@@ -16,13 +19,18 @@ class RickAndMortyApp : Application() {
     }
 
     private fun setupKoin() {
-        initKoin(
-            module {
-                single<Context> { this@RickAndMortyApp }
-                viewModel { CharactersViewModel(get()) }
-                viewModel { LocationsViewModel(get()) }
-                viewModel { EpisodesViewModel(get()) }
-            }
-        )
+        startKoin {
+            androidContext(this@RickAndMortyApp)
+            androidLogger()
+            modules(appModule() + androidModule())
+        }
     }
+
+    private fun androidModule() =
+        module {
+            single<Context> { this@RickAndMortyApp }
+            viewModel { CharactersViewModel(get()) }
+            viewModel { LocationsViewModel(get()) }
+            viewModel { EpisodesViewModel(get()) }
+        }
 }
--- shared/src/commonMain/kotlin/kmm/rickandmorty/app/KoinDI.kt
+++ shared/src/commonMain/kotlin/kmm/rickandmorty/app/KoinDI.kt
@@ -6,15 +6,9 @@ import kmm.rickandmorty.app.data.datasource.remote.RickAndMortyRemoteSource
 import kmm.rickandmorty.app.data.httpClient
 import kmm.rickandmorty.app.data.repository.RickAndMortyDataRepository
 import kmm.rickandmorty.app.domain.repository.RickAndMortyRepository
-import org.koin.core.context.startKoin
-import org.koin.core.module.Module
 import org.koin.dsl.module
 
-fun initKoin(appModule: Module) = startKoin {
-    modules(appModule, commonModule)
-}
-
-val commonModule = module {
+fun appModule() = module {
     single<HttpClient> { httpClient }
     factory<RickAndMortyRemoteSource> { RickAndMortyRemoteDataSource(get()) }
     factory<RickAndMortyRepository> { RickAndMortyDataRepository(get()) }
